c3b7f4f80fa574fc6bb0bcd18f59025d61411d78
alex7771908 <AlexSlu777@gmail.com>
2022-12-05 20:51:19 +0300

    Initial commit

---
+++ platformer/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics; // Debug Console
+using System.Windows.Threading; // For TImer
+
+namespace platformer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //fields
+        private bool bLeft;
+        private bool bRight;
+
+        private int drop = 2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(7);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // gravity
+            double y = Canvas.GetTop(player);
+            Canvas.SetTop(player, y + drop);
+
+            Rect playerCollision = new Rect(Canvas.GetLeft(player), y, player.Width, player.Height);
+
+            var rectangle = MyCanvas.Children.OfType<Rectangle>();
+            
+            foreach(var rect in MyCanvas.Children.OfType<Rectangle>())
+            {
+                if(rect.Tag != null)
+                {
+                    if (rect.Tag.ToString() == "platform")
+                    {
+                        //drop = 0
+                        Rect rectCollision = new Rect(Canvas.GetLeft(rect), Canvas.GetTop(rect), rect.Width, rect.Height);
+
+                        if (playerCollision.IntersectsWith(rectCollision))
+                        {
+                            drop = 0;
+                        }
+                    }
+
+                }
+                
+
+            }
+
+   
+        }
+
+        private void Canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Left || e.Key == Key.A)
+            {
+                bLeft = true;
+            }else if(e.Key == Key.Right || e.Key == Key.D)
+            {
+                bRight = true;
+            }
+
+            Debug.WriteLine($"bleft: {bLeft} bright: {bRight}");
+        }
+
+        private void Canvas_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.A || e.Key == Key.Left)
+            {
+                bLeft = false;
+            }else if( e.Key == Key.Right || e.Key == Key.D)
+            {
+                bRight = false;
+            }
+            Debug.WriteLine($"bleft: {bLeft} bright: {bRight}");
+        }
+    }
+        
+}
